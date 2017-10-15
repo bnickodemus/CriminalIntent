ff89315cbe124fe450396cd7abea427665fc8032
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-15 11:51:53 -0700

    added dateButton and solvedCheckBox to CrimeFragment.java pg 146

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
@@ -7,6 +7,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -16,6 +19,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     public Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,18 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable editable) {
                 // This one too
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 
