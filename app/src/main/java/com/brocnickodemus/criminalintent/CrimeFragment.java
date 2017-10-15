1724903dfd6e06a8808ea2b9e2f284ef29a01525
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-15 11:44:00 -0700

    added onCreateView() and addTextChangedListener() pg 144 and pg 145

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
@@ -2,6 +2,12 @@ package com.brocnickodemus.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by Broc on 10/15/17.
@@ -9,10 +15,36 @@ import android.support.v4.app.Fragment;
 
 public class CrimeFragment extends Fragment {
     public Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }g
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // This one too
+            }
+        });
+
+        return v;
     }
 }
