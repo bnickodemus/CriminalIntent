0e94c714324b8822c222301be97b9e7be55632bf
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-15 20:34:59 -0700

    added crimeLab.java and added getters pg 159, 160

---
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeLab.java
@@ -0,0 +1,42 @@
+package com.brocnickodemus.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Broc on 10/15/17.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+
+        return null;
+    }
+}
