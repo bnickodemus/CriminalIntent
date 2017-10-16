105fce9998ce8ba1af0d804bc0fc5da23e33e806
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-15 23:29:20 -0700

    added onCreateView() pg 172

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeListFragment.java
@@ -1,6 +1,12 @@
 package com.brocnickodemus.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 /**
  * Created by Broc on 10/15/17.
@@ -8,6 +14,15 @@ import android.support.v4.app.Fragment;
 
 public class CrimeListFragment extends Fragment {
 
-    // Nothing yet
+    private RecyclerView mCrimeRecyclerView;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
