5e9312ad58bfc30b07bfe143a8e9bc2180aca0e4
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-23 09:28:25 -0700

    added CrimePagerActivity and view pg 219 and 220

---
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,42 @@
+package com.brocnickodemus.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+/**
+ * Created by Broc on 10/23/17.
+ */
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
+}
