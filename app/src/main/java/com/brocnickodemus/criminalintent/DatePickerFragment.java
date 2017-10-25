917df676a8d0a734d388cfd75947fe8d1e83585b
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-25 00:03:58 -0700

    added dialog_date view and inflated it in DatePickerFragment

--- app/src/main/java/com/brocnickodemus/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by Broc on 10/24/17.
@@ -12,7 +14,11 @@ import android.support.v7.app.AlertDialog;
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
