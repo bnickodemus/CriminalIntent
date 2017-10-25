1afc74d07e2b6c8cad272d2d937d09a39bd3a3a5
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-24 23:58:29 -0700

    added onClickListener to mDateButton

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
@@ -2,6 +2,7 @@ package com.brocnickodemus.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     public Crime mCrime;
     private EditText mTitleField;
@@ -67,7 +69,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
