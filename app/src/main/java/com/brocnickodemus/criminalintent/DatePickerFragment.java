f9b5a71670ee3c26b7487ced0d73e828efdbe388
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-24 23:51:31 -0700

    added date_picker_title and DialogFragment

---
+++ app/src/main/java/com/brocnickodemus/criminalintent/DatePickerFragment.java
@@ -0,0 +1,20 @@
+package com.brocnickodemus.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by Broc on 10/24/17.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
