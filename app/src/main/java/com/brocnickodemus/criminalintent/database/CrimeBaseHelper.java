694e30a484b84e750d22051887108d285e7b734f
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-31 20:02:21 -0700

    added CrimeDbSchema and CrimeBaseHelper

---
+++ app/src/main/java/com/brocnickodemus/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,28 @@
+package com.brocnickodemus.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Broc on 10/31/17.
+ */
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/brocnickodemus/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.brocnickodemus.criminalintent.database;
+
+/**
+ * Created by Broc on 10/31/17.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+    }
+
+    public static final class Cols {
+        public static final String UUID = "uuid";
+        public static final String TITLE = "title";
+        public static final String DATE = "date";
+        public static final String SOLVED = "solved";
+    }
+}
