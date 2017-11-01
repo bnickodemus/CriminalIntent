964411cca0274a785e802ba9247d16da4a4a4e3e
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-31 21:08:48 -0700

    using a cursorWrapper

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeLab.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeLab.java
@@ -2,6 +2,7 @@ package com.brocnickodemus.criminalintent;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.brocnickodemus.criminalintent.database.CrimeBaseHelper;
@@ -56,6 +57,20 @@ public class CrimeLab {
         mDatabase.update(CrimeTable.NAME, values,
                 CrimeTable.Cols.UUID + " = ?",
                 new String[] { uuidString });
+    }
+
+    private Cursor queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null, // columns - null selects all columns
+                whereClause,
+                whereArgs,
+                null, // groupBy
+                null, // having
+                null // orderBy
+        );
+
+        return cursor;
     }
 
     private static ContentValues getContentValues(Crime crime) {
---
+++ app/src/main/java/com/brocnickodemus/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,34 @@
+package com.brocnickodemus.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.brocnickodemus.criminalintent.Crime;
+import com.brocnickodemus.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Broc on 10/31/17.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+        g
+        return crime;
+    }
+}
