55acc80a5513d2fcacd65bec8911e63503bf624e
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-01 00:34:28 -0700

    sending a crime report

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeFragment.java
@@ -33,6 +33,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mReportButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -79,6 +80,7 @@ public class CrimeFragment extends Fragment {
             public void afterTextChanged(Editable editable) {
                 // This one too
             }
+
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
@@ -100,6 +102,18 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setSolved(isChecked);
+            }
+        });
+
+        mReportButton = (Button) v.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+                i = Intent.createChooser(i, getString(R.string.send_report));
+                startActivity(i);
             }
         });
 
