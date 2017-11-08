1a1e87502addaeee29347e2e01de251682eb2758
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-07 22:18:14 -0800

    added alias resource

--- app/src/main/java/com/brocnickodemus/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/brocnickodemus/criminalintent/CrimeListActivity.java
@@ -15,6 +15,6 @@ public class CrimeListActivity extends SingleFragmentActivity{
 
     @Override
     protected int getLayoutResId() {
-        return R.layout.activity_twopane;
+        return R.layout.activity_masterdetail;
     }
 }
